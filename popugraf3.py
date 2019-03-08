259b2a5312d7ecc6def42a6ca7d3a67b13dc0223
Domanski <marcin.domanski@nokia.com>
2019-03-08 14:33:10 +0100

    popugraf more detailed

--- popugraf3.py
+++ popugraf3.py
@@ -1,5 +1,6 @@
 from qgis.core import *
 import qgis.utils
+import time
 
 def totlength(layer):
     a = 0
@@ -31,7 +32,7 @@ def segments(distance,step):
 
 #input line
 registry = QgsProject.instance()
-line = registry.mapLayersByName('maczag')
+line = registry.mapLayersByName('KSS1')
 line = line[0]
 #density map
 density = registry.mapLayersByName('GZM_density_BREC')
@@ -39,34 +40,38 @@ density = density[0]
 #parameters
 distance = 1000 #buffer distance (in meters)
 step = 100 #segments size in meters
-span = 1000 #max value on popugraf
-scale = 30000 #people per span m (needed for offset visualization)
 
 #reproject layer to XY
-processing.run("qgis:reprojectlayer", {'INPUT': line, 'TARGET_CRS': 'EPSG:2180', 'OUTPUT': 'C:/temp/reprojectlayer.shp'})
-line = iface.addVectorLayer('C:/temp/reprojectlayer.shp', "line", "ogr")
+processing.run("qgis:reprojectlayer", {'INPUT': line, 'TARGET_CRS': 'EPSG:2180', 'OUTPUT': 'C:/Temp/reprojectlayer.shp'})
+line = iface.addVectorLayer('C:/Temp/reprojectlayer.shp', "line", "ogr")
 
 #counting whole population in buffer
-processing.run("native:buffer", {'INPUT': line, 'DISTANCE': distance, 'SEGMENTS': 99, 'OUTPUT': 'C:/temp/buffer.shp'})
-processing.run("native:intersection", {'INPUT': 'C:/temp/buffer.shp', 'OVERLAY': density, 'OUTPUT': 'C:/temp/rbuffer.shp'})
-bufor = iface.addVectorLayer('C:/temp/rbuffer.shp', "bufor", "ogr")
+processing.run("native:buffer", {'INPUT': line, 'DISTANCE': distance, 'SEGMENTS': 99, 'OUTPUT': 'C:/Temp/buffer.shp'})
+processing.run("native:intersection", {'INPUT': 'C:/Temp/buffer.shp', 'OVERLAY': density, 'OUTPUT': 'C:/Temp/rbuffer.shp'})
+bufor = iface.addVectorLayer('C:/Temp/rbuffer.shp', "bufor", "ogr")
 
 totpop = 0
+totpop2 = 0
 for feature in bufor.getFeatures():
-    totpop = feature["TOTAL_POP"] + totpop
-print("TOTAL POPULATION: " + str(totpop))
+    area = feature.geometry().area()
+    div = feature["TOTAL_POP"]*area/feature["area"] #for BREC 
+    totpop=totpop+int(div)
 QgsProject.instance().removeMapLayers([bufor.id()])
 
 #counting length of line
 totlen = totlength(line)
+print("TOTAL POPULATION: " + str(totpop))
 print("TOTAL LENGTH: " + str(totlen/1000) + " km")
+print("TOTAL POPULATION PER KM: " + str(int(1000*totpop/totlen)))
 
 #making popugraf
 pattern(totlen,step)
-processing.run("grass7:v.segment", {"input": line, "output": "C:/temp/rule.shp", "rules": "C:/temp/rule.txt", "GRASS_REGION_PARAMETER": density, "GRASS_SNAP_TOLERANCE_PARAMETER": -1, "GRASS_MIN_AREA_PARAMETER": 0, "GRASS_OUTPUT_TYPE_PARAMETER": 1})
-processing.run("native:buffer", {'INPUT': "C:/temp/rule.shp", 'DISTANCE': distance, 'SEGMENTS': 99, 'OUTPUT': 'C:/temp/buffer.shp'})
-processing.run("native:intersection", {'INPUT': 'C:/temp/buffer.shp', 'OVERLAY': density, 'OUTPUT': 'C:/temp/rbuffer.shp'})
-bufor = iface.addVectorLayer('C:/temp/rbuffer.shp', "bufor", "ogr")
+processing.run("grass7:v.segment", {"input": line, "output": "C:/Temp/rule.shp", "rules": "C:/Temp/rule.txt", "GRASS_REGION_PARAMETER": density, "GRASS_SNAP_TOLERANCE_PARAMETER": -1, "GRASS_MIN_AREA_PARAMETER": 0, "GRASS_OUTPUT_TYPE_PARAMETER": 1})
+time.sleep(3)
+processing.run("native:buffer", {'INPUT': "C:/Temp/rule.shp", 'DISTANCE': distance, 'SEGMENTS': 99, 'OUTPUT': 'C:/Temp/abuffer.shp'})
+time.sleep(3)
+processing.run("native:intersection", {'INPUT': 'C:/Temp/abuffer.shp', 'OVERLAY': density, 'OUTPUT': 'C:/Temp/arbuffer.shp'})
+bufor = iface.addVectorLayer('C:/Temp/arbuffer.shp', "bufor", "ogr")
 QgsProject.instance().removeMapLayers([line.id()])
 
 n = int(totlen/step)
@@ -80,7 +85,7 @@ for feature in bufor.getFeatures():
 QgsProject.instance().removeMapLayers([bufor.id()])
 
 #assigning values to segments
-segment = iface.addVectorLayer('C:/temp/rule.shp', "segment", "ogr")
+segment = iface.addVectorLayer('C:/Temp/rule.shp', "segment", "ogr")
 segment.startEditing()
 segment.dataProvider().addAttributes([QgsField("population", QVariant.Int)])
 segment.commitChanges()
